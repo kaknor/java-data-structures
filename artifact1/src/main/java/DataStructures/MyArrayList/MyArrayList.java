9719ae6797ef9ed45ee799a3cfc7b961be8a1f8c
mortician <mortician@morticians-MacBook-Air.local>
2020-09-29 17:05:39 +0200

    [MyArrayList] Implemented first try of size.

--- artifact1/src/main/java/DataStructures/MyArrayList/MyArrayList.java
+++ artifact1/src/main/java/DataStructures/MyArrayList/MyArrayList.java
@@ -1,5 +1,16 @@
 package DataStructures.MyArrayList;
 
+// Information from https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/ArrayList.html
 public class MyArrayList {
+
+    private int[] array_;
+
+	public MyArrayList(int i) {
+        this.array_ = new int[i];
+	}
+
+	public int size() {
+		return this.array_.length;
+	}
     
 }
--- artifact1/src/test/java/DataStructures/MyArrayListTest/MyArrayListTest.java
+++ artifact1/src/test/java/DataStructures/MyArrayListTest/MyArrayListTest.java
@@ -4,9 +4,12 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import DataStructures.MyArrayList.MyArrayList;
+
 public class MyArrayListTest {
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void sizeShouldBe5() {
+        MyArrayList list = new MyArrayList(5);
+        assertTrue(list.size() == 5);
     }
 }

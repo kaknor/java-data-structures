a95e0ff5c89afd6fa0fa11eb4bbf5f21d2875bb3
mortician <mortician@morticians-MacBook-Air.local>
2020-09-30 15:24:21 +0200

    [MyArrayList] implemented add; size; get and set.

--- artifact1/src/main/java/DataStructures/MyArrayList/MyArrayList.java
+++ artifact1/src/main/java/DataStructures/MyArrayList/MyArrayList.java
@@ -1,16 +1,87 @@
 package DataStructures.MyArrayList;
 
+import java.util.Arrays;
+
 // Information from https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/ArrayList.html
 public class MyArrayList {
 
     private int[] array_;
+    private int capacity_;
+    private int size_ = 0;
 
-	public MyArrayList(int i) {
-        this.array_ = new int[i];
-	}
+    public MyArrayList(int capacity) {
+        this.capacity_ = capacity;
+        this.array_ = new int[this.capacity_];
+    }
 
-	public int size() {
-		return this.array_.length;
-	}
-    
+    public MyArrayList(int[] array) {
+        this.capacity_ = array.length;
+        this.size_ = array.length;
+        this.array_ = new int[this.capacity_];
+        for (int i = 0; i < this.size_; ++i) {
+            this.array_[i] = array[i];
+        }
+    }
+
+    public int size() {
+        return this.size_;
+    }
+
+    public int get(int i) {
+        // TODO : Handle negative index in a python like fashion.
+        if (i > this.size_)
+            throw new IndexOutOfBoundsException();
+        return this.array_[i];
+    }
+
+    public Boolean equals(int[] array) {
+        if (this.size_ != array.length)
+            return false;
+
+        for (int i = 0; i < this.size_; ++i)
+            if (this.array_[i] != array[i])
+                return false;
+
+        return true;
+    }
+
+    public void set(int index, int value) {
+        if (index > this.size_)
+            throw new IndexOutOfBoundsException();
+        this.array_[index] = value;
+    }
+
+    public void add(int index, int value) {
+        if (index > this.size_ || index < 0)
+            throw new IndexOutOfBoundsException();
+
+        if (this.capacity_ <= this.size_) {
+            this.size_++;
+            // Reallocate memory
+            this.capacity_ *= 2;
+            int[] tmp = new int[this.capacity_];
+            // Copy memory
+            int i = 0;
+            for (; i < index; ++i) {
+                tmp[i] = this.array_[i];
+            }
+            tmp[index] = value;
+            for (; i < this.size_ - 1; ++i) {
+                tmp[i + 1] = this.array_[i];
+            }
+            this.array_ = tmp;
+        } else {
+            // Move inplace elements of array_ from index to size.
+            for (int i = this.size_; i > index; --i) {
+                 this.array_[i] = this.array_[i - 1];
+            }
+            ++this.size_;
+            this.array_[index] = value;
+        }
+    }
+
+    public void add(int e) {
+        this.add(this.size_, e);
+    }
+
 }
--- artifact1/src/test/java/DataStructures/MyArrayListTest/MyArrayListTest.java
+++ artifact1/src/test/java/DataStructures/MyArrayListTest/MyArrayListTest.java
@@ -8,8 +8,66 @@ import DataStructures.MyArrayList.MyArrayList;
 
 public class MyArrayListTest {
     @Test
-    public void sizeShouldBe5() {
+    public void sizeMustBe0() {
         MyArrayList list = new MyArrayList(5);
-        assertTrue(list.size() == 5);
+        assertTrue(list.size() == 0);
+    }
+
+    @Test
+    public void getMustBe5() {
+        int[] array = {0, 1, 2, 3, 4, 5};
+        MyArrayList list = new MyArrayList(array);
+        assertTrue(list.get(5) == 5);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void getMustThrowIndexOutOfBoundException() {
+        MyArrayList l = new MyArrayList(1);
+        l.get(2);
+    }
+
+    @Test
+    public void comparisonOfArrayAndArrayListMustBeTrue() {
+        int[] array = {1, 2, 3, 4, 5};
+        MyArrayList l = new MyArrayList(array);
+        assertTrue(l.equals(array));
+    }
+
+    @Test
+    public void setIndex0To5() {
+        MyArrayList l = new MyArrayList(1);
+        l.set(0, 5);
+        assertTrue(l.get(0) == 5);
+    }
+
+    @Test
+    public void add3AtPos2() {
+        int[] array = {1, 2, 4, 5};
+        MyArrayList l = new MyArrayList(array);
+        l.add(2, 3);
+
+        int[] arrayAfterAddition = {1, 2, 3, 4, 5};
+        for (int i = 0; i < l.size(); ++i) {
+            assertTrue(arrayAfterAddition[i] == l.get(i));
+        }
+    }
+
+    @Test
+    public void add5CapacityIncreaseMustOccur() {
+        MyArrayList l = new MyArrayList(1);
+        assertTrue(l.size() == 0);
+        l.add(5);
+        l.add(5);
+        assertTrue(l.size() == 2);
+    }
+
+    @Test
+    public void addFirstPosMoveMustOccur() {
+        int[] array = {1, 2};
+        MyArrayList l = new MyArrayList(array);
+        l.add(0, 0);
+        l.add(0, -1);
+        int[] arrayAfterAddition = {-1, 0, 1, 2};
+        assertTrue(l.equals(arrayAfterAddition));
     }
 }
